bae4b9c875b90e2f296ce79fe5323545b7a2a26e
simanamaria <113286111+simanamaria@users.noreply.github.com>
2022-09-27 10:12:02 +0300

    Update Category.java

--- domain/src/main/java/com.coherentsolutions.domain/Category.java
+++ domain/src/main/java/com.coherentsolutions.domain/Category.java
@@ -1,2 +1,15 @@
+package com.coherentsolutions.domain;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Category {
+    String name;
+    List<Product> productList = new ArrayList<Product>();
+
+
+    public Category(String name) {
+        this.name = name;
 }
+
+    }
