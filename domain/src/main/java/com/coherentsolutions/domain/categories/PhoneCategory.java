d425cad3887e2a2e1a3cbc76bddfd65749d857b3
simanamaria <113286111+simanamaria@users.noreply.github.com>
2022-09-28 12:13:54 +0300

    override phonecategory

--- domain/src/main/java/com/coherentsolutions/domain/categories/PhoneCategory.java
+++ domain/src/main/java/com/coherentsolutions/domain/categories/PhoneCategory.java
@@ -21,4 +21,9 @@ public class PhoneCategory extends Category {
     public void setName(String name) {
         this.name = name;
     }
+    
+            @Override
+    public String toString() {
+        return "Phone " + name + "Product list " + productList;
+    }
 }
