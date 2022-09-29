fbae71a9144efc9a44c916228514c9fbc1db6db7
simanamaria <113286111+simanamaria@users.noreply.github.com>
2022-09-29 18:12:43 +0300

    Update MilkCategory.java

--- domain/src/main/java/com/coherentsolutions/domain/categories/MilkCategory.java
+++ domain/src/main/java/com/coherentsolutions/domain/categories/MilkCategory.java
@@ -7,7 +7,7 @@ import com.coherentsolutions.domain.Product;
 import java.util.List;
 
 public class MilkCategory extends Category {
-    String name;
+    private String name;
 
     public MilkCategory(String name, List<Product> productList){
         super(productList);
