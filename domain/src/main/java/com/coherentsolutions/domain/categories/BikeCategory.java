d37d5a723ed2ab0ce3b42ed792b972d44a86b9a0
simanamaria <113286111+simanamaria@users.noreply.github.com>
2022-09-29 18:12:22 +0300

    Update BikeCategory.java

--- domain/src/main/java/com/coherentsolutions/domain/categories/BikeCategory.java
+++ domain/src/main/java/com/coherentsolutions/domain/categories/BikeCategory.java
@@ -7,7 +7,7 @@ import com.coherentsolutions.domain.Product;
 import java.util.List;
 
 public class BikeCategory extends Category {
-    String name;
+   private String name;
 
     public BikeCategory(String name, List<Product> productList){
         super(productList);
