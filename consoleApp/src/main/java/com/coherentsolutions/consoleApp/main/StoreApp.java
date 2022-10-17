bdd50ad2c01d47f3faeb55f5076698507810369c
simanamaria <113286111+simanamaria@users.noreply.github.com>
2022-10-17 19:39:59 +0300

    comparator, methods for sorting and interaction finished

--- consoleApp/src/main/java/com/coherentsolutions/consoleApp/main/StoreApp.java
+++ consoleApp/src/main/java/com/coherentsolutions/consoleApp/main/StoreApp.java
@@ -4,6 +4,9 @@ import com.coherentsolutions.consoleApp.XML.XMLParser;
 import com.coherentsolutions.store.Store;
 import com.coherentsolutions.store.StoreHelper;
 
+
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -15,7 +18,9 @@ public class StoreApp {
 
         Scanner scanner = new Scanner(System.in);
         XMLParser xmlParser = new XMLParser();
-        Map<String, String> map = xmlParser.getMap();
+        Map<String, String> parserMap = xmlParser.getMap();
+        Comparator comparator = (Comparator) new com.coherentsolutions.consoleApp.XML.Comparator();
+        System.out.println("Please enter sort/ stop ");
         while (scanner.hasNext()) {
             String next = scanner.next();
             if (next.equalsIgnoreCase("stop")) {
@@ -23,9 +28,18 @@ public class StoreApp {
             }
             if (next.equalsIgnoreCase("sort")) {
                 System.out.print("Choose sort option: ");
-                map.keySet().forEach((key) -> {
+                parserMap.keySet().forEach((key) -> {
                     System.out.print(key);
                 });
+
+                String sortByField = scanner.next();
+                System.out.println(comparator.sortedProductList(sortByField, store, parserMap));
+            }
+            if (next.equalsIgnoreCase("top")) {
+                Map<String, String> topPrices = new HashMap<>();
+                topPrices.put("price", "desc");
+                System.out.println(comparator.mostExpensive("price", store, topPrices));
+            }
             }
         }
     }
